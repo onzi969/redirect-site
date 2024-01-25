81d6c396a26630eb8990e2c65f85a5fa11fc9568
onzi969 <157653909+onzi969@users.noreply.github.com>
2024-01-25 10:02:36 -0800

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,15 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>CLICK THE LINK!</Title>
+      <h1>CLICK THE LINK!</h1>
       <Counter />
       <p>
         Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+        <a href="https://sites.google.com/my.sduhsd.net/studyhelpwav/home" target="_blank">
+          studyhelp.wav
         </a>{" "}
-        to learn how to build SolidStart apps.
+        ty for using studyhelp.wav!
       </p>
     </main>
   );
